77a7d1934659180f5ba48d6b0afee1f00ab61c89
Sandro Hawke <sandro@hawke.org>
2018-12-29 21:45:46 -0500

    parsing basically works

--- parse-template.js
+++ parse-template.js
@@ -49,7 +49,8 @@ function prepTemplate (t, index, varMap) {
       out.push(XRegExp.escape(part))
     } else {
       const groupName = 'var_' + index + '_' + part.count
-      out.push('(?<' + groupName + '>.+?)')
+      // out.push('(?<' + groupName + '>.+?)')   // needs to be more restrictive or else repeated sentences gobble across boundaries
+      out.push('(?<' + groupName + '>[^.,!]+?)')  // or quoted anything
       varMap[groupName] = part.name
     }
   }
@@ -61,28 +62,31 @@ function mergeTemplates (templates) {
   let index = 0
   const varMap = {}
   for (const t of templates) {
-    const pt = prepTemplate(t, index, varMap)
+    const pt = prepTemplate(t.text, index, varMap)
     re.push('(?<t_' + index + '>' + pt.re + ')')
     index++
   }
   re.push('(?<ws>\\s+)')
   re.push('(?<junk>.+?)')
-  return { re: re.join('|'), varMap }
+  return { re: re.join('|'), varMap, templates }
 }
 
-const tt = [
-  'Hello [string name]!',
-//  'Goodbye [name]',
-//  'I think you might be [age] years old.',
-//'I\'m sure [name] is [age] years old.'
-]
+/*
+  WTF:
 
-const merged = mergeTemplates(tt)
-console.log('RESULT = %O', merged)
+x = 'x\\ '
+// => 'x\\ '
+> new RegExp(x, 'imguy')
+// => SyntaxError: Invalid regular expression: /x\ /: Invalid escape
+new RegExp(x, 'imgy')
+// => /\ /gimy
+
+*/
 
 function parse (merged, text) {
-  const re = new RegExp(merged.re, 'imguy')
-  console.log('re=%o', re)
+  const out = []
+  const re = new RegExp(merged.re, 'imgy')
+  // console.log('re=%o', re)
   while (true) {
     console.log('')
     const m = re.exec(text)
@@ -100,6 +104,7 @@ function parse (merged, text) {
     let tnum
     let line
     for (const [key, value] of Object.entries(m.groups)) {
+      if (!value) continue // to unused ones are set to undefined
       if (key.startsWith('t_')) {
         tnum = parseInt(key.slice(2))
         line = value
@@ -107,13 +112,27 @@ function parse (merged, text) {
       const varName = merged.varMap[key]
       if (varName) b[varName] = value
     }
-    console.log('matched template %d text %o', tnum, line)
-    console.log('  %o', b)
+    // console.log('matched template %d text %o', tnum, line)
+    const t = merged.templates[tnum]
+    console.log('Got %s %o', t.code, b)
+    out.push([t, b])
   }
   console.log('DONE, at pos', re.lastIndex, text.length)
+  return out
 }
 
 // console.log('\n\n %o', parse(merged, 'Hello Sandro! Goodbye, you. Hm'))
 // console.log('\n\n %o', parse(merged, 'x'))
 
-console.log('\n\n %o', parse(merged, 'Hello s! Hello Fred!'))
+
+const tt = [
+  { text: 'Hello [string name]!', code: 'hello' },
+  { text: 'Goodbye [name].' , code: 'bye' },
+  { text: 'I think you are [age] years old.', code: 'age1' },
+  { text: 'I\'m sure [name] is [age] years old.', code: 'age2' }
+]
+
+const merged = mergeTemplates(tt)
+//console.log('RESULT = %O', merged)
+console.log('\n\n %O', parse(merged, 'Hello s! Hello Fred! Goodbye Silly. I think you are foo3 yours old. I think you are 23 years old.'))
+
